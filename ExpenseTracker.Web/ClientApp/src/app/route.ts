8409ee590df7a1501c0cbffb5810a39ccb1b9364
cbowman2006 <cbowman2006@gmail.com>
2022-06-22 09:39:58 -0400

    added expense route

--- ExpenseTracker.Web/ClientApp/src/app/route.ts
+++ ExpenseTracker.Web/ClientApp/src/app/route.ts
@@ -1,6 +1,9 @@
+import { ExpenseComponent } from './expense/expense.component';
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 
 export const appRoutes: Routes = [
-    {path: '', component: HomeComponent, pathMatch: 'full'}
+    {path: '', component: HomeComponent, pathMatch: 'full'},
+    {path: 'expense', component: ExpenseComponent}
+
 ];
